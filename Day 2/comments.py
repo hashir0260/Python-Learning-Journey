c670888bcdb6163abacab64f0851f05e28fe5539
hashir0260 <hashirali0260@gmail.com>
2025-10-31 22:13:35 +0500

    Learned Comments, Escape Sequence and print statements in python

---
+++ Day 2/comments.py
@@ -0,0 +1,22 @@
+#Hy how are you 
+#my name is Hashir Ali
+
+"""Hy how are you i am double 
+line comment """
+
+"""
+This is a multi-line comment
+that explains something big
+"""
+print("Welcome")
+
+#Escape Sequence 
+
+print("Hy i am a boy\nI live in Wahcantt")
+
+print("Hy i am currently in \"6th\" Semester")
+
+print("hy", 6,8)
+
+print("hello",9,33, sep="@")
+print("hehe",8,5, sep="~", end="00000")
