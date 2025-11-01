5e2a8ca9e9260b584a0ecccfea377364e6088af4
hashir0260 <hashirali0260@gmail.com>
2025-11-01 20:31:52 +0500

    Learned Datatypes in Python

---
+++ Day 3/datatypes.py
@@ -0,0 +1,23 @@
+a = 4
+b = "Hashir"
+c = 3.21
+d = True
+e = ["hashir", [1, 5],[45.66]]
+f = ["Banana", "Apple"]
+g = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
