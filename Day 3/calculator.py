1dcdea3d4e6517a8018f483cb1ce68a12b339f9e
hashir0260 <hashirali0260@gmail.com>
2025-11-01 21:26:22 +0500

    Made simple calculator

---
+++ Day 3/calculator.py
@@ -0,0 +1,11 @@
+num1 = 60
+num2 = 54
+
+addition = num1+num2
+print("Addition of two numbers is", addition)
+subtraction = num1-num2
+print("Subtraction of two numbers is", subtraction)
+multiplication = num1*num2
+print("Multiplication of two numbers is", multiplication)
+division = num1/num2
+print("Division of two numbers is", division)
