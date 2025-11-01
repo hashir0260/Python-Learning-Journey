799b41a0fb022c7abf96af0c48f21e4e82790136
hashir0260 <hashirali0260@gmail.com>
2025-11-01 21:25:54 +0500

    Learned Arithmetic operators

---
+++ Day 3/arithmetic.py
@@ -0,0 +1,16 @@
+a = 23
+b = 7
+
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+floor_division = a//b
+Exponential = a**b
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(floor_division)
+print(Exponential)
